cb9f761fe86fb911a2f2922fc41c5ef83a15a3b4
Nhan <ntnhan025@gmail.com>
2023-12-13 15:49:49 +0700

    update screen size

--- src/test/java/erp/base/BaseSetup.java
+++ src/test/java/erp/base/BaseSetup.java
@@ -47,6 +47,8 @@ public class BaseSetup {
         WebDriverManager.chromedriver().clearResolutionCache().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--headless");
+        options.addArguments("--window-size=1920,1080");
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get(url);
